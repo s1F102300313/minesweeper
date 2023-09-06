04cf0d170dbe0d71c95f9a18b6fb485225175140
root <root@LAPTOP-AL8R1EFG>
2023-09-06 13:58:06 +0900

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,199 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [0, -1],
+  [0, 1],
+  [-1, 0],
+  [1, 0],
+  [1, -1],
+  [1, 1],
+  [-1, 1],
+  [-1, -1],
+];
+
 const Home = () => {
+  const normalBoard = [...Array(9)].map(() => [...Array(9)].map(() => 0));
+
+  const [userInput, setUserInput] = useState(normalBoard);
+
+  const [bombMap, setBombMap] = useState(normalBoard);
+
+  const board = normalBoard.map((row) => row.map(() => -1));
+
+  const newUserInput: number[][] = JSON.parse(JSON.stringify(userInput));
+
+  const newBombMap: (0 | 1)[][] = JSON.parse(JSON.stringify(bombMap));
+
+  const isFirst = !userInput.flat().includes(1);
+
+  const isFailed = () =>
+    userInput
+      .map((row, y) => row.map((value, x) => value === 1 && bombMap[y][x] === 1))
+      .flat()
+      .some(Boolean);
+
+  const isWon = () => board.flat().filter((value) => [-1, 9, 10].includes(value)).length === 10;
+
+  const maxMin = (n: number) => Math.max(0, Math.min(8, n));
+
+  const setBomb = (x: number, y: number) => {
+    const zeroToEight = () => Math.floor(Math.random() * 9);
+    let count = 0;
+    while (count < 10) {
+      const nx = zeroToEight();
+      const ny = zeroToEight();
+      if (!(nx === x && ny === y) && newBombMap[ny][nx] === 0) {
+        newBombMap[ny][nx] = 1;
+        count += 1;
+      }
+    }
+  };
+
+  const clickNum = (x: number, y: number) => {
+    if (1 <= board[y][x] && board[y][x] <= 8) {
+      console.log(serchBomb(x, y), serchFlag(x, y));
+      if (serchFlag(x, y) === serchBomb(x, y)) {
+        checkAround(x, y, 'input');
+      }
+    }
+  };
+
+  const serchBomb = (x: number, y: number) =>
+    bombMap
+      .map((row) => row.slice(maxMin(x - 1), maxMin(x + 1) + 1))
+      .slice(maxMin(y - 1), maxMin(y + 1) + 1)
+      .flat()
+      .filter((v) => v === 1).length;
+
+  const serchFlag = (x: number, y: number) =>
+    userInput
+      .map((row) => row.slice(maxMin(x - 1), maxMin(x + 1) + 1))
+      .slice(maxMin(y - 1), maxMin(y + 1) + 1)
+      .flat()
+      .filter((v) => v === 2).length;
+
+  const checkAround = (x: number, y: number, type: 'recursive' | 'input') => {
+    for (const direction of directions) {
+      const dx = direction[0];
+      const dy = direction[1];
+      if (
+        board[y + dy] !== undefined &&
+        board[y + dy][x + dx] !== undefined &&
+        board[y + dy][x + dx] === -1
+      ) {
+        eventHandler(x + dx, y + dy, type);
+      }
+    }
+  };
+
+  const recursive = (x: number, y: number) => {
+    board[y][x] = serchBomb(x, y);
+    if (board[y][x] === 0) {
+      checkAround(x, y, 'recursive');
+    }
+    isBomb(x, y);
+  };
+
+  const eventHandler = (x: number, y: number, type: 'recursive' | 'input') => {
+    if (type === 'recursive') {
+      recursive(x, y);
+    } else {
+      newUserInput[y][x] = 1;
+    }
+  };
+
+  const isBomb = (x: number, y: number) => {
+    if (newBombMap[y][x] === 1 && newUserInput[y][x] === 1) {
+      board[y][x] = 11;
+    }
+  };
+
+  const handleUserInput = (i: number, j: number) => {
+    if (newUserInput[j][i] === 1) {
+      recursive(i, j);
+    }
+    if (newUserInput[j][i] === 2) {
+      board[j][i] = 10;
+    }
+    if (newUserInput[j][i] === 3) {
+      board[j][i] = 9;
+    }
+  };
+  const face = isFailed() ? 14 : isWon() ? 13 : 12;
+  const reset = () => {
+    setUserInput(normalBoard);
+    setBombMap(normalBoard);
+  };
+
+  const clickLeft = (x: number, y: number) => {
+    if (isFirst) {
+      setBomb(x, y);
+      setBombMap(newBombMap);
+    }
+    if (isFailed()) return;
+    if (isWon()) return;
+    if (userInput[y][x] === 0) {
+      newUserInput[y][x] = 1;
+    }
+    clickNum(x, y);
+
+    setUserInput(newUserInput);
+  };
+
+  const clickRight = (x: number, y: number) => {
+    document.getElementsByTagName('html')[0].oncontextmenu = () => false;
+    if (isFailed()) return;
+    if (userInput[y][x] === 0 && board[y][x] === -1) {
+      newUserInput[y][x] = 2;
+    } else {
+      if (userInput[y][x] === 2) {
+        newUserInput[y][x] = 3;
+      } else {
+        newUserInput[y][x] = 0;
+      }
+    }
+    setUserInput(newUserInput);
+  };
+  for (let n = 0; n < 81; n++) {
+    const i = n % 9;
+    const j = Math.floor(n / 9);
+    handleUserInput(i, j);
+  }
+  if (isFailed()) {
+    newBombMap.forEach((row, y) => row.forEach((bomb, x) => bomb === 1 && (board[y][x] = 11)));
+  }
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+      <div className={styles.frame}>
+        <div className={styles.header}>
+          <div
+            className={styles.button}
+            style={{ backgroundPosition: `${7.67 * (face - 1)}%` }}
+            onClick={() => reset()}
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <div style={{ height: 5 }} />
+        <div className={styles.board}>
+          {board.map((row, y) =>
+            row.map((value, x) => (
+              <div
+                key={`${x}-${y}`}
+                onClick={() => clickLeft(x, y)}
+                onContextMenu={() => clickRight(x, y)}
+                className={[-1, 10, 9].includes(value) ? styles.stone : styles.cell}
+                style={{
+                  backgroundPosition: `${7.67 * (value - 1)}%`,
+                  backgroundColor:
+                    isFailed() && userInput[y][x] === 1 && bombMap[y][x] === 1 ? '#f00' : '#bbb',
+                }}
+              />
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Home;
